dfe8ee815ebbd8c3730925df86c3bc241a521c98
MyaGya <38939015+MyaGya@users.noreply.github.com>
2020-11-06 03:19:24 +0900

    파일 업로드 폼 및 마진 전체 적용

--- cache_server/cache_video/views.py
+++ cache_server/cache_video/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 def main(request):
     '''
-    pybo 목록 출력
+    동영상 출력 페이지
     '''
     # 입력 파라미터
     page = request.GET.get('page', '1')  # 페이지
@@ -18,3 +18,11 @@ def main(request):
 
     #context = {'question_list': page_obj}
     return render(request, 'cache_video/video_list.html')
+
+def upload(request):
+    '''
+    페이지 출력
+    '''
+    # 입력 파라미터
+    page = request.GET.get('page', '1')  # 페이지
+    return render(request, 'cache_video/upload.html')
