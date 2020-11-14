2d7e8fc0f8c6854ac9382f972d1ce6ac73cdda64
MyaGya <38939015+MyaGya@users.noreply.github.com>
2020-11-14 23:35:37 +0900

    유투브 다운로드 기능 구현

--- cache_server/cache_server/upload.py
+++ cache_server/cache_server/upload.py
@@ -1,3 +1,17 @@
+from pytube import YouTube
 
 
-print('test')
+# YouTube('https://www.youtube.com/watch?v=riI4FGbKN9k').streams.first().download()
+# print(YouTube('https://www.youtube.com/watch?v=riI4FGbKN9k').streams)
+def download_youtube():
+    data = YouTube('https://www.youtube.com/watch?v=riI4FGbKN9k').streams
+    for s in data:
+        if s.resolution == "1080p":
+            s.download()
+            return
+    data[0].download()
+
+
+#download_youtube()
+
+
