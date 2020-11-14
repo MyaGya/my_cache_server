8f6ea420f6327d7b2f2e597d3799ee23223c9077
MyaGya <38939015+MyaGya@users.noreply.github.com>
2020-11-14 23:35:26 +0900

    DB 데이터 타입 변경(제목, 업로드 시간 추가)

--- cache_server/cache_video/models.py
+++ cache_server/cache_video/models.py
@@ -8,19 +8,29 @@ from django.contrib.auth.models import User
 
 
 class TrackingUrl(models.Model):
+    '''
+    사용자가 추적하고자 하는 유투브 채널
+    '''
     user = models.ForeignKey(User, on_delete=models.CASCADE)    # 등록한 유저
     url = models.CharField(max_length=100)                      # 등록한 url
 
 
 class LocalUrl(models.Model):
+    '''
+    해당 유투브 채널의
+    '''
     tracking_url = models.ForeignKey(TrackingUrl, on_delete=models.CASCADE)  # 지정되어있는 트래킹 url
-    url = models.CharField(max_length=100)                                   # 실제 다운로드 하고자 하는 url
+    subject = models.CharField(max_length=100)                               # 제목
     local = models.CharField(max_length=100)                                 # 다운하고자 하는 파일의 local 위치
-    thumbnail = models.CharField(max_length=100)                             # 썸네일 파일의 local 위치
-    upload_date = models.DateTimeField()
+    thumbnail = models.CharField(max_length=100, blank=True)                 # 썸네일 파일의 local 위치
+    upload_date = models.DateTimeField()                                     # 업로드 시간
+    running_time = models.DateTimeField(max_length=100)                      # 동영상 길이
 
 
 class UploadedFile(models.Model):
+    '''
+    개별 업로드 파일의 데이터
+    '''
     user = models.ForeignKey(User, on_delete=models.CASCADE)    # 파일을 올린 유저
     subject = models.CharField(max_length=100)                  # 제목
     upload_time = models.DateTimeField()                        # 업로드된 시간
