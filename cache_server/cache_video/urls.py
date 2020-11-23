5b392e20edbece8950d131e0ad4dab11b855da82
MyaGya <38939015+MyaGya@users.noreply.github.com>
2020-11-23 13:53:43 +0900

    메인 페이지 비로그인시 출력 화면 변경

--- cache_server/cache_video/urls.py
+++ cache_server/cache_video/urls.py
@@ -3,14 +3,15 @@ from . import views
 
 app_name = 'cache_video'
 urlpatterns = [
-    path('', views.go_main, name='go_main'),  # 메인 페이지
+    path('', views.go_main, name='go_main'), # 메인 페이지
     path('main/', views.main, name='main'),  # 메인 페이지
+    path('main_detail/<int:tracking_url_id>', views.main_detail, name='main_detail'),  # 메인 페이지의 상세 보기
     path('upload/', views.upload, name='upload'),  # 업로드 페이지
-    path('upload_check/', views.upload_check, name='upload_check'),  # 내 비디오
-    path('upload_check/video_delete/<int:uploadedfile_id>', views.video_delete, name='video_delete'),  # 비디오 삭제
-    path('upload_check/url_delete/<int:urltracking_id>', views.url_delete, name='url_delete'),  # url 삭제
-    path('upload_check/add_url', views.add_url, name='add_url'),  # url 등록
-    path('main_detail/<int:tracking_url_id>', views.main_detail, name='main_detail')  # 메인 페이지의 상세보기
+    path('upload_check/', views.upload_check, name='upload_check'),  # 업로드 파일 확인 페이지
+    path('upload_check/video_delete/<int:uploadedfile_id>', views.video_delete, name='video_delete'),  # 비디오 삭제 기능
+    path('upload_check/url_delete/<int:urltracking_id>', views.url_delete, name='url_delete'),  # url 삭제 기능
+    path('upload_check/add_url', views.add_url, name='add_url'),  # url 등록 페이지
+
 
     # path('<int:question_id>/', views.main_detail, name='main_detail'),                              # 상세보기 페이지
     # path('answer/create/', views.question_create, name='question_create'),                # 질문 생성 페이지
